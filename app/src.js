16f67a0cf660adc9139e37873f81e5204387ccf6
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-10 13:50:30 +0200

    client side added

--- app/src.js
+++ app/src.js
@@ -3,10 +3,12 @@ const path = require("path")
 const hbs = require("hbs")
 const app = express()
 
+app.use(express.static(path.join(__dirname, "../clientSide/public")))
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "../clientSide/views"))
+hbs.registerPartials(path.join(__dirname, "../clientSide/layouts"))
 
-
-
-app.get("/", (req, res) => res.send("Home page"));
+app.get("/", (req, res) => res.render("allBooks"));
 
 
 
