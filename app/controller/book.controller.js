be77a8afe8b1b99dacfe36f9dcf78fa133f4571c
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-10 17:46:49 +0200

    all books view completed

--- app/controller/book.controller.js
+++ app/controller/book.controller.js
@@ -3,12 +3,10 @@ class Book {
 
     static allBooks = (req, res) => {
         const books = dealHelper.readFromJSON()
-        console.log(books)
-        books.forEach(element => {
-            console.log(element)
-        });
-        res.render("allBooks", { books })
-
+        res.render("allBooks", {
+            pageTitle: "All book", books,
+            hasBooks: books.length
+        })
     }
 
 
