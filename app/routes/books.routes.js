0512b8f1d231769fbe25015a08d56f24891a330d
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-10 14:25:53 +0200

    crud routes added

--- app/routes/books.routes.js
+++ app/routes/books.routes.js
@@ -2,6 +2,12 @@ const router = require("express").Router()
 
 router.get("/", (req, res) => res.render("allBooks"));
 
+router.get("/addBook", (req, res) => res.send("addbook"))
+router.get("/book/:id", (req, res) => res.send(`book id ${req.params.id}`))
+router.get("/edit/:id", (req, res) => res.send(`book id ${req.params.id}`))
+router.get("/delete/:id", (req, res) => res.send(`book id ${req.params.id}`))
+
+
 
 
 module.exports = router
